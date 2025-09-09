378179c0e0917b4bcc7eacce0b6e7cbf44916fc7
Joash Chee <joash@scorelessmusic.com>
2025-09-09 17:31:55 +0800

    Implemented most detection methods

--- main.lua
+++ main.lua
@@ -1,8 +1,22 @@
--- for love 0.9.1, patched for 11.5
-
+-- for love 11.5
+-- on iphone (landscape), screen seems to be 830x368 (hard to touch corners x:30px y20px)
+-- iphone landscape, camera island 60px, bottom swipe bar 40px 
 
 function love.load()
 
+	-- flags
+	mouseDetected = false
+
+	-- variables
+	mouse = {}
+	mouse.x = 0
+	mouse.y = 0
+	mouse.button = 0
+	mouse.clicks = 0
+	keypressed = {}
+	keypressedHistory = ""	
+	holdToQuit = 0
+	
 	-- codepage 437, 8x16 px, 80x30 chars (640x480)
 	monoFont = love.graphics.newFont("Mx437_IBM_VGA_8x16.ttf", 16)
 	FONT_WIDTH = 8
@@ -11,63 +25,154 @@ function love.load()
 	print(monoFont:getWidth("█"))
 	print(monoFont:getHeight())
 
-
-  helpText = "Type away! -- "
-	keyValue = "nil"
-	scanValue = ""
-	textValue = ""
-	stringValue = ""
-
+	-- joystick code
+	local axisCount = {}
+	local buttonCount = {}
+	local hatCount = {}
+  local joystick = love.joystick.getJoysticks() -- table
+	if joystick ~= nil then -- not empty table, joystick(s) detected
+		for i = 1,#joystick do
+			axisCount[i] = joystick[i]:getAxisCount() -- number
+			buttonCount[i] = joystick[i]:getButtonCount() -- number
+			hatCount[i] = joystick[i]:getHatCount() -- number
+		end
+	end
 
 end
 
 
 function love.textinput(t)
-    textValue = t
-    stringValue = stringValue .. t
+	-- callback on text input
 end
 
 
 function love.keypressed(key, scancode, isrepeat)
-	-- report the key's key
-	keyValue = key
-	scanValue = scancode
 	-- trap 'escape' unicode 27 to quit
-	if key == "escape" then
-		love.filesystem.write( "textdump.txt", stringValue )		
-		love.event.quit()
+	-- if key == "escape" then
+	--		love.filesystem.write( "textdump.txt", stringValue )		
+	--		love.event.quit()
+	-- end
+	local keyPressedBefore = false
+	for _, value in pairs(keypressed) do
+		if type(value) == "string" and value == scancode then
+			keyPressedBefore = true
+		end
+	end
+	if keyPressedBefore == false then -- scancode not found in table
+		-- add scancode to keypressed table
+		table.insert(keypressed, scancode)
+		table.sort(keypressed)
 	end
 end
 
+function love.keyreleased(key, scancode)
+	-- reset holdToQuit
+	holdToQuit = 0
+end
 
-function love.joystickpressed(joystick,button)
-	-- detect joystick input
+
+function love.joystickpressed(joystick,button) -- joystick is object, button is number
+	-- joystick button presses detected
 end
 
 -- love.mousepressed( x, y, button, istouch, presses )
 -- number presses
 -- The number of presses in a short time frame and small area, used to simulate double, triple clicks
-function love.mousepressed(x, y, button, istouch)
-	-- detect mouse presses
+function love.mousepressed( x, y, button, istouch, presses )
+	-- mouse button presses detected
+	mouse.button = button
+	mouse.clicks = presses
 end
-
+function love.mousemoved( x, y, dx, dy, istouch )
+	-- mouse movement detected
+	mouseDetected = true
+	mouse.x = love.mouse.getX()
+	mouse.y = love.mouse.getY()
+end
 -- love.touchpressed( id, x, y, dx, dy, pressure )
 -- number pressure
 -- The amount of pressure being applied. Most touch screens aren't pressure sensitive, in which case the pressure will be 1.
+function love.mousereleased( x, y, button, istouch, presses )
+	-- reset holdToQuit
+	holdToQuit = 0
+end
+
 function love.touchpressed( id, x, y, dx, dy, pressure )
 	-- detect touches
 end
 
 function love.draw()
-    -- set font before draw text
-    love.graphics.setFont(monoFont)
+	-- set font before draw text
+  love.graphics.setFont(monoFont)
     
-    -- concat each char as they are typed
-    love.graphics.printf(helpText, 0, 0, 800)
-    love.graphics.print("keypressed  (key) = " .. keyValue, 150, 150)
-    love.graphics.print("keypressed (scan) = " .. scanValue, 150, 200)
-    love.graphics.print("textinput  (text) = " .. textValue, 150, 250)
-    if textValue == "™" then
-	    love.graphics.print("Trade Mark pressed (Opt-2)", 150, 450)
-    end
+  -- draw joysticks data
+  love.graphics.print("--[ Joysticks ]--",FONT_WIDTH*0,FONT_HEIGHT*0)
+	if joystick ~= nil then -- not empty table, joystick(s) detected
+		for i = 1,#joystick do
+			love.graphics.print("Joystick "..i..": "..axisCount[i].." axes,"..buttonCount[i].." buttons, "..hatCount[i].." hats",FONT_WIDTH*0,FONT_HEIGHT*(i))
+		end
+	else
+		love.graphics.print("No joystick detected",FONT_WIDTH*0,FONT_HEIGHT*1)
+  end  
+
+	-- draw mouse data
+  love.graphics.print("--[ Mouse ]--",FONT_WIDTH*0,FONT_HEIGHT*5)
+	if mouseDetected then
+		love.graphics.print("Mouse detected, x:"..mouse.x.." y:"..mouse.y,FONT_WIDTH*0,FONT_HEIGHT*6)
+		if mouse.button > 0 then
+			love.graphics.print("Mouse button "..mouse.button.." pressed, "..mouse.clicks.." clicks",FONT_WIDTH*0,FONT_HEIGHT*7)		
+		end
+	else
+		love.graphics.print("No mouse detected",FONT_WIDTH*0,FONT_HEIGHT*6)
+	end
+
+	-- draw touch data
+  love.graphics.print("--[ Touches ]--",FONT_WIDTH*0,FONT_HEIGHT*9)
+	if love.touch.getTouches() ~= nil then
+		love.graphics.print("#getTouches = "..#love.touch.getTouches(),FONT_WIDTH*0,FONT_HEIGHT*10)
+		local touches = love.touch.getTouches()
+    for i, id in ipairs(touches) do
+      local x, y = love.touch.getPosition(id)
+      love.graphics.circle("fill", x, y, 20)
+			love.graphics.print("Touch "..i.." x:"..math.floor(x).." y:"..math.floor(y),FONT_WIDTH*0,FONT_HEIGHT*(10+i))
+    end	
+  else
+		love.graphics.print("getTouches == nil",FONT_WIDTH*0,FONT_HEIGHT*10)	
+	end	
+
+	-- draw keyboard data
+  love.graphics.print("--[ Keyboard ]--",FONT_WIDTH*40,FONT_HEIGHT*0)
+  love.graphics.print("#keypressed = "..#keypressed,FONT_WIDTH*40,FONT_HEIGHT*1)
+	keypressedHistory = ""
+	for _, value in pairs(keypressed) do
+		keypressedHistory = keypressedHistory .. value .. " "
+	end
+	love.graphics.printf(keypressedHistory, FONT_WIDTH*40, FONT_HEIGHT*2, FONT_WIDTH*40, "left")
+
+	-- draw holdToQuit
+	-- love.graphics.print("holdToQuit :"..tostring(holdToQuit),FONT_WIDTH*35,FONT_HEIGHT*28)
+  love.graphics.print("--[ hold anything to quit ]--",FONT_WIDTH*((80-30)/2),FONT_HEIGHT*28)
+	love.graphics.printf(holdToQuit,FONT_WIDTH*0,FONT_HEIGHT*29,FONT_WIDTH*80, "center")
+
+end
+
+
+
+function love.update(dt)
+
+-- joystickcount = love.joystick.getJoystickCount( ) -- number
+-- direction = Joystick:getHat( hat ) -- constant: c d l ld lu r rd ru u
+
+-- x, y = love.touch.getPosition( id )
+-- pressure = love.touch.getPressure( id )
+
+	-- for holdToQuit feature
+	if love.mouse.isDown( 1, 2, 3) then
+		holdToQuit = holdToQuit + dt
+	end
+
+	if holdToQuit >= 3 then
+		love.event.quit()
+	end
+
 end
